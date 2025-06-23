2ffbc97e1c84bceb2f997a055e9cf4f22b245d49
sbk283 <sbkcoding@gmail.com>
2025-06-23 12:59:04 +0900

    [docs] 함수의 이해

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,17 @@
-let numA: number = 5;
-let numB: number = 8;
-
-const resultPlus: number = numA + numB;
-const resultMinus: number = numA - numB;
-const resultDevide: number = numA / numB;
-const resultMulti: number = numA * numB;
-
-/**
- * 사칙연산 함수
- * - 숫자 2개를 입력하시면 결과가 나옵니다.
- * - 기호는 4가지(+, -, * /)를 사용하실 수 있습니다.
- *
- * 사용예시
- * ```javascript
- * const result:number = calc(5, 4, "+");
- * ```
- * @param {number} a
- * @param {number} b
- * @param {string} sign
- * @returns {number}
- */
-
-function calc (a:number, b:number, sign:string):number {
-    let result: number = 0;
-    
-    switch (sign) {
-        case ("+"):
-            result = a + b;
-            break;
-        case ("-"):
-            result = a - b;
-            break;
-        case ("*"):
-            result = a * b;
-            break;
-        case ("/"):
-            result = (a !== 0 && b !== 0) ? a / b : 0
-            // 0으로 나누기 방지
-            break;
+function 사용자인증(auth:boolean, fn: () => void) {
+    if (auth) {
+        fn();
+    } else {
+        console.log("권한이 없습니다.");
     }
-    return result;
 };
 
+// 이용권한이 있습니다. 출력
+사용자인증(true, () => {
+    console.log("이용권한이 있습니다.");
+});
+
+// 권한이 없습니다. 출략
+사용자인증(false, () => {
+    console.log("이용권한이 없습니다.");
+});
