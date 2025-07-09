511d70ed928cd9810c0ec606ddc59ad3ab3c30a1
sbk283 <sbkcoding@gmail.com>
2025-07-09 10:58:37 +0900

    [docs] type 과 interface 선택기준

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-interface Animal {
+interface 약속 {
   name: string;
 }
-const ani: Animal = {
-  name: "홍길동",
-};
-interface Dog extends Animal {
-  bark(): void;
+
+class Person implements 약속 {
+  name: string;
 }
-const dog: Dog = {
-  name: "멍멍이",
-  bark: () => {
-    console.log("멍멍");
-  },
-};
-interface Cat extends Animal {
-  meow(): void;
-}
-const cat: Cat = {
-  name: "야옹이",
-  meow: () => {
-    console.log("야옹");
-  },
-};
-interface Person extends Animal {
-  say(): void;
-}
-const iu: Person = {
-  name: "아이유",
-  say: () => {
-    console.log("안녕하세요");
-  },
-};
