dc585572594f5feda5947a109c564ec656af6cd4
sbk283 <sbkcoding@gmail.com>
2025-07-15 09:35:28 +0900

    [docs] 공부과정 푸쉬

--- src/SignUpForm.tsx
+++ src/SignUpForm.tsx
@@ -1,25 +1,25 @@
-import React from "react";
-// 전역(window) 자리
-interface SignUpData {
-  username: string;
-  password: string;
-  email: string;
-  agree: boolean;
-}
-function SignUpForm() {
-  // ts 자리
-  const [formData, setFormData] = useState({});
-  // tsx 자리
-  return (
-    <form>
-      <h2>회원가입폼</h2>
-      <input type="text" name="username" />
-      <input type="password" name="userpass" />
-      <input type="text" name="useremail" />
-      <input type="checkbox" name="useragree" />
-      <button type="submit">가입하기</button>
-    </form>
-  );
-}
+// import React from "react";
+// // 전역(window) 자리
+// interface SignUpData {
+//   username: string;
+//   password: string;
+//   email: string;
+//   agree: boolean;
+// }
+// function SignUpForm() {
+//   // ts 자리
+//   const [formData, setFormData] = useState<SignUpData>({});
+//   // tsx 자리
+//   return (
+//     <form>
+//       <h2>회원가입폼</h2>
+//       <input type="text" name="username" />
+//       <input type="password" name="userpass" />
+//       <input type="text" name="useremail" />
+//       <input type="checkbox" name="useragree" />
+//       <button type="submit">가입하기</button>
+//     </form>
+//   );
+// }
 
-export default SignUpForm;
+// export default SignUpForm;
